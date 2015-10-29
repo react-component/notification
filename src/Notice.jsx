0743c861715e240dde2e393ad4c7670b00206e03
Benjy Cui <benjytrys@gmail.com>
2015-10-29 09:34:51 +0800

    style: update code style to please ESLint

---
+++ src/Notice.jsx
@@ -0,0 +1,72 @@
+import React from 'react';
+import {classSet} from 'rc-util';
+
+const Notice = React.createClass({
+  propTypes: {
+    duration: React.PropTypes.number,
+    onClose: React.PropTypes.func,
+    children: React.PropTypes.any,
+  },
+
+  getDefaultProps() {
+    return {
+      onEnd() {
+      },
+      duration: 1.5,
+      style: {
+        right: '50%',
+      },
+    };
+  },
+
+  componentDidMount() {
+    this.clearCloseTimer();
+    if (this.props.duration) {
+      this.closeTimer = setTimeout(()=> {
+        this.close();
+      }, this.props.duration * 1000);
+    }
+  },
+
+  componentDidUpdate() {
+    this.componentDidMount();
+  },
+
+  componentWillUnmount() {
+    this.clearCloseTimer();
+  },
+
+  clearCloseTimer() {
+    if (this.closeTimer) {
+      clearTimeout(this.closeTimer);
+      this.closeTimer = null;
+    }
+  },
+
+  close() {
+    this.clearCloseTimer();
+    this.props.onClose();
+  },
+
+  render() {
+    const props = this.props;
+    const componentClass = `${props.prefixCls}-notice`;
+    const className = {
+      [`${componentClass}`]: 1,
+      [`${componentClass}-closable`]: props.closable,
+      [props.className]: !!props.className,
+    };
+    return (
+      <div className={classSet(className)} style={props.style}>
+        <div className={`${componentClass}-content`}>{this.props.children}</div>
+        {props.closable ?
+          <a tabIndex="0" onClick={this.close} className={`${componentClass}-close`}>
+            <span className={`${componentClass}-close-x`}></span>
+          </a> : null
+        }
+      </div>
+    );
+  },
+});
+
+export default Notice;
---
+++ src/Notification.jsx
@@ -0,0 +1,98 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Animate from 'rc-animate';
+import {createChainedFunction, classSet} from 'rc-util';
+import Notice from './Notice';
+
+let seed = 0;
+const now = Date.now();
+
+function getUuid() {
+  return 'rcNotification_' + now + '_' + (seed++);
+}
+
+const Notification = React.createClass({
+  getDefaultProps() {
+    return {
+      prefixCls: 'rc-notification',
+      animation: 'fade',
+      style: {
+        'top': 65,
+        left: '50%',
+      },
+    };
+  },
+
+  getInitialState() {
+    return {
+      notices: [],
+    };
+  },
+
+  getTransitionName() {
+    const props = this.props;
+    let transitionName = props.transitionName;
+    if (!transitionName && props.animation) {
+      transitionName = `${props.prefixCls}-${props.animation}`;
+    }
+    return transitionName;
+  },
+
+  add(notice) {
+    const key = notice.key = notice.key || getUuid();
+    const notices = this.state.notices;
+    if (!notices.filter((v) => v.key === key).length) {
+      this.setState({
+        notices: notices.concat(notice),
+      });
+    }
+  },
+
+  remove(key) {
+    const notices = this.state.notices.filter((notice) => {
+      return notice.key !== key;
+    });
+    this.setState({
+      notices: notices,
+    });
+  },
+
+  render() {
+    const props = this.props;
+    const noticeNodes = this.state.notices.map((notice)=> {
+      const onClose = createChainedFunction(this.remove.bind(this, notice.key), notice.onClose);
+      return (<Notice prefixCls={props.prefixCls} {...notice} onClose={onClose}>{notice.content}</Notice>);
+    });
+    const className = {
+      [props.prefixCls]: 1,
+      [props.className]: !!props.className,
+    };
+    return (
+      <div className={classSet(className)} style={props.style}>
+        <Animate transitionName={this.getTransitionName()}>{noticeNodes}</Animate>
+      </div>
+    );
+  },
+});
+
+Notification.newInstance = function(properties) {
+  const props = properties || {};
+  const div = document.createElement('div');
+  document.body.appendChild(div);
+  const notification = ReactDOM.render(<Notification {...props}/>, div);
+  return {
+    notice(noticeProps) {
+      notification.add(noticeProps);
+    },
+    removeNotice(key) {
+      notification.remove(key);
+    },
+    component: notification,
+    destroy() {
+      ReactDOM.unmountComponentAtNode(div);
+      document.body.removeChild(div);
+    },
+  };
+};
+
+export default Notification;
