1991a255a10ccc00fc05e7ebe00b5e25b8cba6b3
afc163 <afc163@gmail.com>
2017-04-27 14:53:26 +0800

    fix react createClass and PropTypes warning

--- src/Notice.jsx
+++ src/Notice.jsx
@@ -1,25 +1,24 @@
-import React, { PropTypes } from 'react';
+import React, { Component } from 'react';
 import classNames from 'classnames';
+import PropTypes from 'prop-types';
 
-const Notice = React.createClass({
-  propTypes: {
+export default class Notice extends Component {
+  static propTypes = {
     duration: PropTypes.number,
     onClose: PropTypes.func,
     children: PropTypes.any,
-  },
+  };
 
-  getDefaultProps() {
-    return {
-      onEnd() {
-      },
-      onClose() {
-      },
-      duration: 1.5,
-      style: {
-        right: '50%',
-      },
-    };
-  },
+  static defaultProps = {
+    onEnd() {
+    },
+    onClose() {
+    },
+    duration: 1.5,
+    style: {
+      right: '50%',
+    },
+  };
 
   componentDidMount() {
     if (this.props.duration) {
@@ -27,23 +26,23 @@ const Notice = React.createClass({
         this.close();
       }, this.props.duration * 1000);
     }
-  },
+  }
 
   componentWillUnmount() {
     this.clearCloseTimer();
-  },
+  }
 
-  clearCloseTimer() {
+  clearCloseTimer = () => {
     if (this.closeTimer) {
       clearTimeout(this.closeTimer);
       this.closeTimer = null;
     }
-  },
+  }
 
-  close() {
+  close = () => {
     this.clearCloseTimer();
     this.props.onClose();
-  },
+  }
 
   render() {
     const props = this.props;
@@ -63,7 +62,5 @@ const Notice = React.createClass({
         }
       </div>
     );
-  },
-});
-
-export default Notice;
+  }
+}
--- tests/index.js
+++ tests/index.js
@@ -1,5 +1,5 @@
 const React = require('react');
-const TestUtils = require('react-addons-test-utils');
+const TestUtils = require('react-dom/test-utils');
 const expect = require('expect.js');
 const Notification = require('../');
 
