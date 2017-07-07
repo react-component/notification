9f1e40bfd1a862d6c37dbf452cd227178c92e51e
afc163 <afc163@gmail.com>
2017-07-07 14:59:03 +0800

    Revert #17
    Fix
    https://github.com/ant-design/ant-design/issues/5869#issuecomment-313400042

--- src/Notification.jsx
+++ src/Notification.jsx
@@ -79,13 +79,9 @@ class Notification extends Component {
       [props.prefixCls]: 1,
       [props.className]: !!props.className,
     };
-    const animateProps = {};
-    if (this.state.notices.length <= 1) {
-      animateProps.component = '';
-    }
     return (
       <div className={classnames(className)} style={props.style}>
-        <Animate transitionName={this.getTransitionName()} {...animateProps}>{noticeNodes}</Animate>
+        <Animate transitionName={this.getTransitionName()}>{noticeNodes}</Animate>
       </div>
     );
   }
