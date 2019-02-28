470aceea752d4c32972f26bf6be391bf97db4140
narro <501xs05@163.com>
2019-02-28 11:49:27 +0800

    fix: 2 space indent.

--- index.d.ts
+++ index.d.ts
@@ -1,42 +1,42 @@
 import React from "react";
 
 export interface INotificationProps {
-    /** prefix class name for notification container */
-    prefixCls?: string;
-    /** additional style for notification container. */
-    style?: object;
-    /** function returning html node which will act as notification container */
-    getContainer?(): HTMLElement;
+  /** prefix class name for notification container */
+  prefixCls?: string;
+  /** additional style for notification container. */
+  style?: object;
+  /** function returning html node which will act as notification container */
+  getContainer?(): HTMLElement;
 }
 
 export interface INoticeProps {
-    /** content of notice */
-    content: React.ReactNode;
-    /** id of this notice */
-    key?: string;
-    /** whether show close button */
-    closable?: boolean;
-    /** called when notice close */
-    onClose?(): void;
-    /** after duration of time, this notice will disappear.(seconds) */
-    duration?: number;
-    /** additional style for single notice node. */
-    style?: object;
-    /** max notices show, drop first notice if exceed limit */
-    maxCount?: number;
-    /** specific the close icon. */
-    closeIcon?: React.ReactNode;
+  /** content of notice */
+  content: React.ReactNode;
+  /** id of this notice */
+  key?: string;
+  /** whether show close button */
+  closable?: boolean;
+  /** called when notice close */
+  onClose?(): void;
+  /** after duration of time, this notice will disappear.(seconds) */
+  duration?: number;
+  /** additional style for single notice node. */
+  style?: object;
+  /** max notices show, drop first notice if exceed limit */
+  maxCount?: number;
+  /** specific the close icon. */
+  closeIcon?: React.ReactNode;
 }
 
 export interface INotification {
-    notice(props: INoticeProps): void;
-    /**
-     * remove single notice with specified key
-     * @param key id of this notice
-     */
-    removeNotice(key: string): void;
-    /** destroy current notification */
-    destroy(): void;
+  notice(props: INoticeProps): void;
+  /**
+   * remove single notice with specified key
+   * @param key id of this notice
+   */
+  removeNotice(key: string): void;
+  /** destroy current notification */
+  destroy(): void;
 }
 
 /**
