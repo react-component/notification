4bddb8ce5f0c08106bad54365746707d3f1a55ba
zombiej <smith3816@gmail.com>
2021-03-02 16:36:51 +0800

    fix: hooks should support update

--- src/useNotification.tsx
+++ src/useNotification.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
-import Notification, { NoticeFunc, NoticeContent } from './Notification';
+import type { NoticeFunc, NoticeContent } from './Notification';
+import type Notification from './Notification';
 import Notice from './Notice';
 
 export default function useNotification(
@@ -9,14 +10,28 @@ export default function useNotification(
   const [elements, setElements] = React.useState<React.ReactElement[]>([]);
 
   function notify(noticeProps: NoticeContent) {
+    let firstMount = true;
     notificationInstance.add(noticeProps, (div, props) => {
       const { key } = props;
 
-      if (div && !createdRef.current[key]) {
+      if (div && (!createdRef.current[key] || firstMount)) {
         const noticeEle = <Notice {...props} holder={div} />;
         createdRef.current[key] = noticeEle;
-        setElements(originElements => [...originElements, noticeEle]);
+
+        setElements((originElements) => {
+          const index = originElements.findIndex((ele) => ele.key === props.key);
+
+          if (index === -1) {
+            return [...originElements, noticeEle];
+          }
+
+          const cloneList = [...originElements];
+          cloneList[index] = noticeEle;
+          return cloneList;
+        });
       }
+
+      firstMount = false;
     });
   }
 
