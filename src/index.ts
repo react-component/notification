36859f4caf6c33179e0c48454aef36fb82861a1a
zombiej <smith3816@gmail.com>
2022-06-07 11:03:13 +0800

    chore: export Notice Component

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import useNotification from './useNotification';
+import Notice from './Notice';
 import type { NotificationAPI, NotificationConfig } from './useNotification';
 
-export { useNotification };
+export { useNotification, Notice };
 export type { NotificationAPI, NotificationConfig };
