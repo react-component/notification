92c76a5169a2f4bdb5e91de3b6a81caf1ef2a0d9
kiner-tang(文辉) <1127031143@qq.com>
2023-01-08 10:37:34 +0800

    chore: update dumi config

--- .dumirc.ts
+++ .dumirc.ts
@@ -1,7 +1,14 @@
 import { defineConfig } from 'dumi';
+import path from 'path';
 
 export default defineConfig({
+  alias: {
+    'rc-notification$': path.resolve('src'),
+    'rc-notification/es': path.resolve('src'),
+  },
+  favicons: ['https://avatars0.githubusercontent.com/u/9441414?s=200&v=4'],
   themeConfig: {
     name: 'Notification',
+    logo: 'https://avatars0.githubusercontent.com/u/9441414?s=200&v=4',
   },
 });
